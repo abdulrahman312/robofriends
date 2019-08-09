b23aef22b1db119e6e806b866ce857785a7fcec5
Khulood <khankhulood99@gmail.com>
2019-08-09 06:33:03 -0700

    Updates

---
+++ precache-manifest.3e91791821f40233c1c363d319437060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6784f774c24c94a5433a7178c86e5d6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4e30f393333ce8291947",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8e62eccb398d07a618bc",
+    "url": "/robofriends/static/css/main.d7b7a633.chunk.css"
+  },
+  {
+    "revision": "4e30f393333ce8291947",
+    "url": "/robofriends/static/js/2.299af970.chunk.js"
+  },
+  {
+    "revision": "8e62eccb398d07a618bc",
+    "url": "/robofriends/static/js/main.9c1ead5d.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
